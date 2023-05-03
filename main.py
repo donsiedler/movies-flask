8f78e41342ff9d735ab8953c25ea9c7348476e90
Artur Siedlerski <don.siedler@gmail.com>
2023-05-03 18:22:45 +0200

    Setup SQLite db

--- main.py
+++ main.py
@@ -7,10 +7,20 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
-
+# create the extension
+db = SQLAlchemy()
+# create the app
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+# configure the SQLite database, relative to the app instance folder
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////movies.db"
+# initialize the app with the extension
+db.init_app(app)
 Bootstrap5(app)
+
+
+class Movie(db.Model):
+    pass
 
 
 @app.route("/")
